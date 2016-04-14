4933df8202f7f45398451ce98e8642df117ed01f
maximal1st <maximl1st@gmail.com>
2016-04-15 00:16:36 +0300

    Create uk-ua.js

---
+++ src/flipclock/js/lang/uk-ua.js
@@ -0,0 +1,27 @@
+(function($) {
+
+  /**
+   * FlipClock Ukrainian Language Pack
+   *
+   * This class will used to translate tokens into the Ukrainian language.
+   *
+   */
+
+  FlipClock.Lang.Ukrainian = {
+
+    'years'   : 'років',
+    'months'  : 'місяців',
+    'days'    : 'днів',
+    'hours'   : 'годин',
+    'minutes' : 'хвилин',
+    'seconds' : 'секунд'
+
+  };
+
+  /* Create various aliases for convenience */
+
+  FlipClock.Lang['uk']      = FlipClock.Lang.Ukrainian;
+  FlipClock.Lang['uk-ua']   = FlipClock.Lang.Ukrainian;
+  FlipClock.Lang['ukrainian']  = FlipClock.Lang.Ukrainian;
+
+}(jQuery));
